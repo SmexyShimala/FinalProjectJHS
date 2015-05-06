245bfc554550067f77e6c466571afcd1b51d1706
SmexyShimala <jshimala@saintjoe.edu>
2015-05-06 14:46:25 -0500

    Update Mammal.java

--- Mammal.java
+++ Mammal.java
@@ -1,13 +1,14 @@
-public class mammal {	
-	public String species;
-	public String fur;
-	public String thumbs;
 
-
-
-	Mammal(String thisSpecies, String thisFur, String thisThumbs) {
-	species = thisSpecies;
-	fur = thisFur;
-	thumbs = thisThumbs;
+public class Mammal {	
+	String name;	
+	public Mammal (String name)	
+	
+	{this.name = name;}
+	
+public static void main (String args[])	{
+	animal a = new dog(" dog");
+	animal b = new cat(" cat");
+	a.Mykindis();		
+	b.Mykindis();	
 	}
-}
+} 
