58bdb038590008d66e6a3cab9f21b98b941f4a3f
SmexyShimala <jshimala@saintjoe.edu>
2015-05-06 14:58:18 -0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,1 +1,17 @@
+public class human {	
+	public String ethnicity;
+	public String hair;
+	public String name;
+	public String eyecolor;
+	public int birthyear;
 
+
+
+	Human(String thisEthnicity, String thisHair, String thisName, String thisEyecolor, int thisBirthyear) {
+  ethnicity = thisEthnicity;
+  hair = thisHair;
+  name = thisName;
+  eyecolor = thisEyecolor;
+  birthyear = thisBirthyear;
+	}
+}
