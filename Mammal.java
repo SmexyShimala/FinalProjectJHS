313a94968210fae1e8a1f087bc13207b46f98633
SmexyShimala <jshimala@saintjoe.edu>
2015-05-06 14:42:15 -0500

    Update Mammal.java

--- Mammal.java
+++ Mammal.java
@@ -1,1 +1,13 @@
+public class mammal {	
+	public String species;
+	public String fur;
+	public String thumbs;
 
+
+
+	Mammal(String thisSpecies, String thisFur, String thisThumbs) {
+	species = thisSpecies;
+	fur = thisFur;
+	thumbs = thisThumbs;
+	}
+}
